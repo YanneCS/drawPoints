b42858c26285491c74907e4b06ee10ef4c3e9ec7
Anne <anney829@gmail.com>
2019-04-09 22:12:28 -0400

    first Commit

---
+++ drawPoints.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+y = [1,2,4,3,4,5,6,4,8,7,12,11,11,8,5,6,7,10,6,4]
+
+xMin = []
+yMin = []
+
+for z in range (0, len(y)):
+    if y[z] == min(y):
+        xMin.append(x[z])
+        yMin.append(y[z])
+
+maxX = max(x)
+maxY = max(y)
+minX = min(x)
+minY = min(y)
+
+plt.plot(x, y, marker = 'o', linestyle = '-')
+plt.plot(xMin, yMin, linewidth = 4, linestyle = '-')
+plt.axis([(minX - 1), (maxX + 1), (minY - 1), (maxY + 1)])
+
+
+plt.show()
